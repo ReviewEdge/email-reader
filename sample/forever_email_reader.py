e254132a5e9f152930d86e800b9c1fa89076cbb2
ReviewEdge <reviewedge56@gmail.com>
2020-04-12 14:09:13 -0400

    fixed import file paths

--- sample/forever_email_reader.py
+++ sample/forever_email_reader.py
@@ -1,5 +1,5 @@
-import email_reader
-import use_files
+import email_reader_repo
+from furtherpy.sample import files_tool
 import time
 
 
@@ -10,11 +10,11 @@ print("\n[forever_email_reader] Starting email_reader.py")
 while 1:
     # checks if any music is playing
     try:
-        email_reader.main()
+        email_reader_repo.main()
 
     except Exception as e:
         # saves error file
-        use_files.basic_write_file("email_reader_crash_report", "email_reader crashed with the exception: " + str(e))
+        files_tool.basic_write_file("email_reader_crash_report", "email_reader crashed with the exception: " + str(e))
 
         print("\n[forever_email_reader] The error: '" + str(e) +
               "' occurred while running email_reader.py.\nTrying again...\n")
