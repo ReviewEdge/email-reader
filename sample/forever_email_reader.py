f2cca42faa7b0923734a6dd3c3e1e718e4628310
ReviewEdge <reviewedge56@gmail.com>
2020-06-14 16:29:39 -0400

    added no emails today catch

--- sample/forever_email_reader.py
+++ sample/forever_email_reader.py
@@ -12,11 +12,15 @@ while 1:
         email_reader.main()
 
     except Exception as e:
-        # saves error file
-        files_tool.basic_write_file("email_reader_crash_report", "email_reader crashed with the exception: " + str(e))
+        if str(e) == "list index out of range":
+            print("\n[forever_email_reader] No emails have been yet received today.\nChecking again...\n")
 
-        print("\n[forever_email_reader] The error: '" + str(e) +
-              "' occurred while running email_reader.py.\nTrying again...\n")
+        else:
+            # saves error to file
+            files_tool.basic_write_file("email_reader_crash_report", "email_reader crashed with the exception: " + str(e))
+
+            print("\n[forever_email_reader] The error: '" + str(e) +
+                  "' occurred while running email_reader.py.\nTrying again...\n")
 
         # waits to restart loop
         time.sleep(30)
